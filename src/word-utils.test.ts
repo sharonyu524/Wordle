fb94642513cac8d6eef731ccb1e34e92fea86baf
sharonyu524 <xy3@wellesley.edu>
2023-12-30 14:07:45 -0500

    Implemented compute guess function. Need to fix the algo when the same letter appears multiple times

--- src/word-utils.test.ts
+++ src/word-utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it} from 'vitest';
-import {getRandomWord} from './word-utils';
+import {LetterState, computeGuess, getRandomWord} from './word-utils';
 import{render, screen} from './test/test-utils'
 
 describe('word-utils', () => {
@@ -10,3 +10,40 @@ describe('word-utils', () => {
   });
 
 });
+
+describe('computeGuess', () => {
+    it('works with LetterState,', () => {
+      
+      expect(computeGuess("boost", "basic")).toEqual([
+        LetterState.Match,
+        LetterState.Miss,
+        LetterState.Miss,
+        LetterState.Present,
+        LetterState.Miss
+      ]);
+    });
+
+    it('works with all matched,', () => {
+      
+        expect(computeGuess("boost", "boost")).toEqual([
+          LetterState.Match,
+          LetterState.Match,
+          LetterState.Match,
+          LetterState.Match,
+          LetterState.Match,
+        ]);
+      });
+      // TODO: need to fix the algorithm when the same letter appears multiple times 
+      it('works with all matched,', () => {
+      
+        expect(computeGuess("allol", "smelt")).toEqual([
+          LetterState.Miss,
+          LetterState.Present,
+          LetterState.Miss,
+          LetterState.Miss,
+          LetterState.Miss,
+        ]);
+      });
+  
+  });
+  
