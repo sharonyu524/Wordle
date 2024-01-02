148b8f9327037b6a7520bf5efcb36bc044ded1a0
sharonyu524 <xy3@wellesley.edu>
2024-01-02 17:11:49 -0500

    implemented zustand to keep track of states

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import WordRow from './wordRow';
+import { useState } from 'react';
+import {useStore} from './store';
+import WordRow from './WordRow';
 
+const GUESS_LENGTH = 6;
 export default function App() {
+  const state = useStore();
+
+  const [guess,setGuess] = useState('');
+
+  const rows = [...state.guesses];
+
+  if (rows.length > 0){
+    rows.push(guess);
+  }
+  const numberOfGuessesRemaining = GUESS_LENGTH - rows.length; 
+  
+
 
   return (
 
       <div className="mx-auto w-96">
         <header className='border-b border-gray-500 pb-2 mb-2'>
           <h1 className="text-4xl text-center"> Wordle </h1>
+          <div>
+          <input type = "text" className="w-1/2 p-2 border-2 border-gray-500" value = {guess} onChange = {(e)=>setGuess(e.target.value)}></input>
+          </div>
         </header>
-
+       
         <main className='grid grid-rows-6 gap-4'>
           <WordRow letters="he" />
           <WordRow letters="hell"/>
---
+++ src/store.ts
@@ -0,0 +1,46 @@
+import { create } from 'zustand'
+import { persist} from 'zustand/middleware'
+import { getRandomWord } from './word-utils'
+
+interface StoreState{
+    answer: string;
+    guesses: string[];
+    addGuess:(guess:string) => void;
+}
+// export const useStore = create<StoreState>(
+//   persist(
+//     (set) => ({
+//       answer: getRandomWord(),
+//       guesses: ['hello','solar','penny'],
+//       addGuess:(guess:string) => {
+//         set((state)=> ({
+//             guesses:[...state.guesses,guess],
+//         }));
+//       }),
+//     }),
+
+//     {
+//       name: 'wordle', // name of the item in the storage (must be unique)
+//     },
+//   ),
+// )
+
+export const useStore = create <StoreState>()(
+    persist(
+      (set) => ({
+        answer: getRandomWord(),
+        guesses:['hello','penny'],
+        addGuess: (guess:string) => {
+           set(state => ({
+            guesses:[...state.guesses,guess],
+           }));
+        }
+      }),
+      {
+        name: 'wordle', // name of the item in the storage (must be unique)
+        
+      },
+    ),
+  )
+
+  // useStore.persist.clearStorage();
