2c2fa6c16b89e8d82e9d35638e15ad6625b6e195
sharonyu524 <xy3@wellesley.edu>
2024-01-18 22:08:08 -0500

    Fix import statement in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from './App'
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root')
 )
